7d2e7ca6ca8dc0c131a45c313c562c74e6f31a86
YaswanthManoharan <yaswanthtarun1@gmail.com>
2025-01-04 18:02:57 +0530

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Footer() {
   return (
     <footer className="bg-white text-yellow-900 py-8 mt-12">
@@ -17,19 +19,29 @@ export default function Footer() {
             <h3 className="text-lg font-medium mb-2">Quick Links</h3>
             <ul className="space-y-2">
               <li>
-                <a href="/" className="text-yellow-700 hover:text-yellow-900 transition">Home</a>
+                <Link href="/" className="text-yellow-700 hover:text-yellow-900 transition">
+                  Home
+                </Link>
               </li>
               <li>
-                <a href="/products" className="text-yellow-700 hover:text-yellow-900 transition">Products</a>
+                <Link href="/products" className="text-yellow-700 hover:text-yellow-900 transition">
+                  Products
+                </Link>
               </li>
               <li>
-                <a href="/reviews" className="text-yellow-700 hover:text-yellow-900 transition">Reviews</a>
+                <Link href="/reviews" className="text-yellow-700 hover:text-yellow-900 transition">
+                  Reviews
+                </Link>
               </li>
               <li>
-                <a href="/contact-us" className="text-yellow-700 hover:text-yellow-900 transition">Contact Us</a>
+                <Link href="/contact-us" className="text-yellow-700 hover:text-yellow-900 transition">
+                  Contact Us
+                </Link>
               </li>
               <li>
-                <a href="/feedback" className="text-yellow-700 hover:text-yellow-900 transition">Feedback</a>
+                <Link href="/feedback" className="text-yellow-700 hover:text-yellow-900 transition">
+                  Feedback
+                </Link>
               </li>
             </ul>
           </div>
@@ -38,18 +50,18 @@ export default function Footer() {
           <div className="text-center">
             <h3 className="text-lg font-medium mb-2">Follow Us</h3>
             <div className="flex justify-center space-x-6">
-              <a href="https://facebook.com" className="text-yellow-700 hover:text-yellow-900 transition">
+              <Link href="https://facebook.com" className="text-yellow-700 hover:text-yellow-900 transition">
                 <i className="fab fa-facebook-f"></i> {/* Add appropriate icon */}
-              </a>
-              <a href="https://twitter.com" className="text-yellow-700 hover:text-yellow-900 transition">
+              </Link>
+              <Link href="https://twitter.com" className="text-yellow-700 hover:text-yellow-900 transition">
                 <i className="fab fa-twitter"></i> {/* Add appropriate icon */}
-              </a>
-              <a href="https://instagram.com" className="text-yellow-700 hover:text-yellow-900 transition">
+              </Link>
+              <Link href="https://instagram.com" className="text-yellow-700 hover:text-yellow-900 transition">
                 <i className="fab fa-instagram"></i> {/* Add appropriate icon */}
-              </a>
-              <a href="https://linkedin.com" className="text-yellow-700 hover:text-yellow-900 transition">
+              </Link>
+              <Link href="https://linkedin.com" className="text-yellow-700 hover:text-yellow-900 transition">
                 <i className="fab fa-linkedin-in"></i> {/* Add appropriate icon */}
-              </a>
+              </Link>
             </div>
           </div>
         </div>
